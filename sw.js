5c3724b21515080d3abb5d056e63062e7ab99b0b
tingting810430-pixel <tingting810430@gmail.com>
2026-08-08 23:21:59 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'shift-calendar-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// 安裝並快取資源
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// 啟動並清理舊快取
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 攔截請求，優行使用快取（實現無網路離線開啟）
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
